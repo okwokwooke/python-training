f03ffeb0a2a6566a02cc966ba2249f542990f2b3
Oke Okwokwo <oke.okwokwo@postgrad.manchester.ac.uk>
2023-03-27 15:28:20 +0100

    spelling chnage

--- catchment/models.py
+++ catchment/models.py
@@ -16,7 +16,7 @@ def read_variable_from_csv(filename):
     of data - only one of which will be read.
 
     :param filename: Filename of CSV to load
-    :return: 2D array of given variable. Index will be dates,
+    :return: 2d array of given variable. Index will be dates,
              Columns will be the individual sites
     """
     dataset = pd.read_csv(filename, usecols=['Date', 'Site', 'Rainfall (mm)'])
